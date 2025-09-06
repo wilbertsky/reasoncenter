4184d885b8f02b600be6abc520711e4cad1babd4
Will Scharff <willscharff@icloud.com>
2025-09-06 13:27:06 -0700

    Adds Symfony\UX\Turbo\TurboBundle to symfony config bundles.

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
+    Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
 ];
